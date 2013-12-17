4362115f8ebcbc59eda43933570a3a28e6e0ef2f
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2013-12-17 14:18:48 +0400

    Progress & Stars

--- progress/progress.js
+++ progress/progress.js
@@ -12,13 +12,13 @@
      * @type {number}
      * @private
      */
-    this._max = +element.dataset.max || 100;
+    this._max = +element.dataset['max'] || 100;
     /**
      * Current value.
      * @type {number}
      * @private
      */
-    this._value = +element.dataset.value || 0;
+    this._value = +element.dataset['value'] || 0;
     /**
      * Progressbar element.
      * @type {HTMLElement}
@@ -32,7 +32,7 @@
      */
     this._bar = document.createElement('div');
     this._bar.classList.add('progress-bar');
-    this._element.appendChild(this._bar);
+    element.appendChild(this._bar);
 
     // Set current value on start.
     this.set(this._value);
@@ -48,7 +48,7 @@
       throw new RangeError('Значение должно быть от 0 до ' + this._max);
     }
     this._value = +value;
-    this._element.dataset.value = +value;
+    this._element.dataset['value'] = +value;
     this._bar.style.left = Math.floor(this._value / this._max * 100) + '%';
   };
 
@@ -57,19 +57,13 @@
    * @type {Function}
    */
   window.Progressbar = Progressbar;
-})(window);
 
 
-// Run it
-(function(window) {
-  'use strict';
+  // Run it
   window.addEventListener('load', function() {
     var containers = window.document.querySelectorAll('.progress');
     for (var ci = 0, cl = containers.length; ci < cl; ci++) {
-      var bar = new window.Progressbar(containers[ci]);
-
-      // Test it (first - error, second - works)
-      setTimeout(bar.set.bind(bar, 800), 4000);
+      new window.Progressbar(containers[ci]);
     }
   }, false);
 })(window);
--- stars/stars.js
+++ stars/stars.js
@@ -1,18 +1,85 @@
 (function(window) {
   'use strict';
+
+  /**
+   * Stars.
+   * @param {HTMLElement} element
+   * @constructor
+   */
+  var Stars = function(element) {
+    /**
+     * Stars count.
+     * @type {number}
+     * @private
+     */
+    this._count = +element.dataset['count'] || 5;
+    /**
+     * Current rate.
+     * @type {number}
+     * @private
+     */
+    this._value = +element.dataset['value'] || 0;
+    /**
+     * Stars list.
+     * @type {Array}
+     * @private
+     */
+    this._starsList = [];
+
+    // Create wrapper for stars
+    var wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    element.appendChild(wrapper);
+
+    // Create stars
+    var i = this._count + 1;
+    while (--i) {
+      var star = document.createElement('div');
+      star.classList.add('stars-star');
+      star.title = i.toString();
+      wrapper.appendChild(star);
+      this._starsList[i] = star;
+
+      // Init events
+      star.addEventListener('click', (function(Container, index) {
+        return Container.set.bind(Container, index);
+      })(this, i), false);
+    }
+
+    // Set current value on start
+    this.set(this._value);
+  };
+
+  /**
+   * Set stars value.
+   * @param {number} value
+   * @public
+   */
+  Stars.prototype.set = function(value) {
+    if (value > this._count || value < 0) {
+      throw new Error('Значение должно быть в интервале от 0 до ' + this._count);
+    }
+    if (this._value) {
+      this._starsList[this._value].classList.remove('active');
+    }
+    this._value = +value;
+    if (this._value) {
+      this._starsList[this._value].classList.add('active');
+    }
+  };
+
+  /**
+   * @export
+   * @type {Function}
+   */
+  window.Stars = Stars;
+
+
+  // Run it
   window.addEventListener('load', function() {
     var containers = window.document.querySelectorAll('.stars');
     for (var ci = 0, cl = containers.length; ci < cl; ci++) {
-      containers[ci].addEventListener('click', function(e) {
-        if (!e.target.classList.contains('stars-star')) {
-          return;
-        }
-        var active = this.querySelector('.active');
-        if (active) {
-          active.classList.remove('active');
-        }
-        e.target.classList.add('active');
-      }, false);
+      new Stars(containers[ci]);
     }
   }, false);
 })(window);
