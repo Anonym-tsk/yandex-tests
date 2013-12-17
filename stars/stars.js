4aa1c48f8bf0b975c389f30ba1cd2908c8dfce29
Nikolay Vasilchuk <Anonym.tsk@gmail.com>
2013-12-17 12:08:47 +0400

    Stars

--- stars/stars.js
+++ stars/stars.js
@@ -1,24 +1,18 @@
 (function(window) {
   'use strict';
   window.addEventListener('load', function() {
-    // Обходим все блоки со звездами
     var containers = window.document.querySelectorAll('.stars');
     for (var ci = 0, cl = containers.length; ci < cl; ci++) {
-      // Обходим все звезды в блоке
-      var stars = containers[ci].querySelectorAll('.stars-star');
-      for (var si = 0, sl = stars.length; si < sl; si++) {
-        stars[si].addEventListener('click', (function(ci) {
-          return function() {
-            // При клике снимаем класс active в этом блоке
-            var active = containers[ci].querySelector('.active');
-            if (active) {
-              active.classList.remove('active');
-            }
-            // И ставим active на звезду по которой кликнули
-            this.classList.add('active');
-          };
-        })(ci), false);
-      }
+      containers[ci].addEventListener('click', function(e) {
+        if (!e.target.classList.contains('stars-star')) {
+          return;
+        }
+        var active = this.querySelector('.active');
+        if (active) {
+          active.classList.remove('active');
+        }
+        e.target.classList.add('active');
+      }, false);
     }
   }, false);
 })(window);
